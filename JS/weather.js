67d00375b618dde7c822a47b969a61af9b9a8c86
JUNG <97083703+Jung2312@users.noreply.github.com>
2023-02-06 00:07:51 +0900

    Add files via upload
    기능 완성

---
+++ JS/weather.js
@@ -0,0 +1,27 @@
+import { weather_KEY } from "./API_KEY.js";
+
+const weatherIconImg = document.querySelector("#weather-img");
+
+function onGeoOK(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_KEY}&units=metric&lang=kr`;
+  fetch(url) // call url
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherIcon = data.weather[0].icon;
+      const weatherIconAdrs = `../img/${weatherIcon}.png`;
+
+      const weather = document.querySelector("#weather-temp");
+      weather.innerText = `${data.name}, ${data.main.temp}°C`;
+      weatherIconImg.src = weatherIconAdrs;
+    });
+}
+
+function onGeoError() {
+  alert("날씨를 찾을 수 없습니다.");
+  weatherIconImg.src = "../img/unknown.png";
+}
+
+// PC의 현재 위치 불러옴
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
