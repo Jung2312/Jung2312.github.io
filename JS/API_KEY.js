36257f00462fa88301272651452c9809a799c9e8
JUNG <97083703+Jung2312@users.noreply.github.com>
2023-02-06 00:03:14 +0900

    기능 완성
    카카오 책 검색 api 추가, 메모장 기능 추가, 각 메뉴 클릭 시 다른 메뉴 숨기기

--- JS/API_KEY.js
+++ JS/API_KEY.js
@@ -1,1 +1,2 @@
-export const API_KEY = "55d0f0b2f63264b3079894777e5ff4b4";
+export const weather_KEY = "55d0f0b2f63264b3079894777e5ff4b4";
+export const book_KEY = "7daa4418aafff4de0d99127dd8b6b309";
--- JS/weather.js
+++ JS/weather.js
@@ -1,15 +1,17 @@
-import { API_KEY } from "./API_KEY.js";
+import { weather_KEY } from "./API_KEY.js";
+
+const weatherIconImg = document.querySelector("#weather-img");
 
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weather_KEY}&units=metric&lang=kr`;
   fetch(url) // call url
     .then((response) => response.json())
     .then((data) => {
       const weatherIcon = data.weather[0].icon;
       const weatherIconAdrs = `../img/${weatherIcon}.png`;
-      const weatherIconImg = document.querySelector("#weather-img");
+
       const weather = document.querySelector("#weather-temp");
       weather.innerText = `${data.name}, ${data.main.temp}°C`;
       weatherIconImg.src = weatherIconAdrs;
@@ -18,6 +20,7 @@ function onGeoOK(position) {
 
 function onGeoError() {
   alert("날씨를 찾을 수 없습니다.");
+  weatherIconImg.src = "../img/unknown.png";
 }
 
 // PC의 현재 위치 불러옴
